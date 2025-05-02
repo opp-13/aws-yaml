d515345142562e61b1427c675a7c05d1fccb43dd
S6John <john_sungho@naver.com>
2025-05-02 09:48:48 +0900

    fix: add export

--- prac1/eks-env.bash
+++ prac1/eks-env.bash
@@ -6,11 +6,11 @@ VPC_STACK="eks-vpc-stack"
 outputs=$(aws cloudformation describe-stacks --stack-name $VPC_STACK --query "Stacks[0].Outputs" --output json)
 
 # 개별 값 추출
-VPC_ID=$(echo $outputs | jq -r '.[] | select(.OutputKey=="VPCId").OutputValue')
-PRIVATE_SUBNET_1_ID=$(echo $outputs | jq -r '.[] | select(.OutputKey=="PrivateSubnet1Id").OutputValue')
-PRIVATE_SUBNET_2_ID=$(echo $outputs | jq -r '.[] | select(.OutputKey=="PrivateSubnet2Id").OutputValue')
-PRIVATE_DB_SUBNET_1_ID=$(echo $outputs | jq -r '.[] | select(.OutputKey=="PrivateSubnetDB1Id").OutputValue')
-PRIVATE_DB_SUBNET_2_ID=$(echo $outputs | jq -r '.[] | select(.OutputKey=="PrivateSubnetDB2Id").OutputValue')
+export VPC_ID=$(echo $outputs | jq -r '.[] | select(.OutputKey=="VPCId").OutputValue')
+export PRIVATE_SUBNET_1_ID=$(echo $outputs | jq -r '.[] | select(.OutputKey=="PrivateSubnet1Id").OutputValue')
+export PRIVATE_SUBNET_2_ID=$(echo $outputs | jq -r '.[] | select(.OutputKey=="PrivateSubnet2Id").OutputValue')
+export PRIVATE_DB_SUBNET_1_ID=$(echo $outputs | jq -r '.[] | select(.OutputKey=="PrivateSubnetDB1Id").OutputValue')
+export PRIVATE_DB_SUBNET_2_ID=$(echo $outputs | jq -r '.[] | select(.OutputKey=="PrivateSubnetDB2Id").OutputValue')
 
 # 결과 출력
 echo "VPC_STACK: $VPC_STACK"
@@ -20,3 +20,4 @@ echo "PRIVATE_SUBNET_2_ID (AZ-c): $PRIVATE_SUBNET_2_ID"
 echo "PRIVATE_DB_SUBNET_1_ID (AZ-d): $PRIVATE_DB_SUBNET_1_ID"
 echo "PRIVATE_DB_SUBNET_2_ID (AZ-d): $PRIVATE_DB_SUBNET_2_ID"
 
+envsubst < eks-prac1.yaml > eks-prac-env-add.yaml
